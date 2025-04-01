43ced5133387fdfdc547124adc40ee1301418c12
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-04-01 14:40:24 -0300

    add authentication backends; configure django-allauth for user authentication and update requirements

--- places/settings.py
+++ places/settings.py
@@ -87,6 +87,11 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
