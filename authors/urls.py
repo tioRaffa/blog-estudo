9692b57d15c82b3eef68e96f3ea6658c9583f242
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-04-03 16:29:52 -0300

    implement publication management; create PublicationCrud view for handling user publications, update pagination and table templates for displaying published and unpublished posts

--- authors/urls.py
+++ authors/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from authors.view import RegisterView, UpdateView, teste
+from authors.view import RegisterView, UpdateView, PublicationCrud
 app_name = 'authors'
 
 urlpatterns = [
     path('profile/', UpdateView.as_view(), name='profile'),
     path('register/', RegisterView.as_view(), name='register_user'),
-    path('publications/', teste, name='publications_crud'),
+    path('publications/', PublicationCrud.as_view(), name='publications_crud'),
 ]
--- authors/view/publications_settings.py
+++ authors/view/publications_settings.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
+from core.models import PostModel
+from django.views.generic import ListView
 
-def teste(request):
-    return render(request, 'pages/publications.html')
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+
+
+@method_decorator(
+    login_required(
+        login_url='places:index',
+        redirect_field_name='next'
+    ), name='dispatch'
+)
+class PublicationCrud(ListView):
+    model = PostModel
+    template_name = 'pages/publications.html'
+    context_object_name = 'post_published'
+    paginate_by = 4
+    ordering = '-id'
+    
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(author=self.request.user, is_published=True)
+        
+        return queryset
+
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        not_published = PostModel.objects.filter(author=self.request.user, is_published=False)
+        context.update({
+            'post_not_published': not_published
+        })
+        
+        return context
+    
+    
+    
+    
